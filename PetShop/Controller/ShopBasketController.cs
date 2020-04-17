328a3753e8f46192be0bf1752c0b4b88f2e80f17
Natalia Filipek <nataliafilipek@MacBook-Air-Natalia.local>
2020-04-17 07:07:43 +0200

    Add item to shopping basket work

--- PetShop/Controller/ShopBasketController.cs
+++ PetShop/Controller/ShopBasketController.cs
@@ -8,18 +8,18 @@ namespace PetShop.Controller
 {
     public class ShopBasketController
     {
-        ShoppingBasket Basket = new ShoppingBasket();
+        
         List<ShoppingBasket> basket = new List<ShoppingBasket>();
         DAOProduct productFromDB = new DAOProduct();
 
         public List<ShoppingBasket> addProductToBasket(int Id)
         {
-           
+            ShoppingBasket Basket = new ShoppingBasket();
             List<string> purchaseProdcut = productFromDB.GetProductByID(Id);
             Product product = new Product(Convert.ToInt32(purchaseProdcut[0]), purchaseProdcut[1], Convert.ToInt32(purchaseProdcut[2]), Convert.ToInt32(purchaseProdcut[3]));
             Basket.ProductName = product.ProductName;
             Basket.PriceProduct = product.Price;
-            Basket.Quantity = product.Quantity;
+            Basket.Quantity = 1;
             basket.Add(Basket);
 
             return basket;
@@ -37,6 +37,7 @@ namespace PetShop.Controller
 
         public ShoppingBasket removeProductToBasket(int Id)
         {
+            ShoppingBasket Basket = new ShoppingBasket();
             Product product = new Product(1, "Proszek", 23, 2);
             Basket.ProductName = product.ProductName;
             Basket.PriceProduct = product.Price;
